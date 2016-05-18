3491c030278f36a674e853edc35b39349a744e8c
Lemao81 <jr.berlin@web.de>
2016-05-18 18:38:23 +0200

    Update DbHelper.java

--- DbHelper.java
+++ DbHelper.java
@@ -1,6 +1,7 @@
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import static java.lang.String.*;
 
 public class DbHelper extends SQLiteOpenHelper
 {
@@ -15,12 +16,24 @@ public class DbHelper extends SQLiteOpenHelper
     @Override
     public void onCreate(SQLiteDatabase db)
     {
+        StringBuilder sb = new StringBuilder();
 
+        sb.append(format("CREATE TABLE %s", ));
+        sb.append(" (");
+        sb.append(format("%s INTEGER PRIMARY KEY AUTOINCREMENT,", ._ID));
+        sb.append(format("%s TEXT NOT NULL,", ));
+        sb.append(format("%s INTEGER NOT NULL,", ));
+        sb.append(format("%s REAL NOT NULL,", ));
+        sb.append(");");
+        
+        // sb.append(format("CONSTRAINT UNIQUE (%s)", NYSExchangeEntry.COL_SYMBOL));
+
+        db.execSQL(sb.toString());
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
     {
-
+        db.execSQL(format("DROP TABLE IF EXISTS %s ", .TABLE_NAME));
     }
 }

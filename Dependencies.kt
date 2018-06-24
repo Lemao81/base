806919015e9a075d3db25a8b9027f7f09198a4b6
Jürgen Reiser <jr.berlin@web.de>
2018-06-24 18:16:18 +0200

    arch navigation

--- Dependencies.kt
+++ Dependencies.kt
@@ -11,10 +11,10 @@ object Versions {
     const val fabricPlugin = "1.24.4"
     const val hugoPlugin = "1.2.1"
     
-    const val andutils = "1.0.24"
-    const val jutils = "1.0.6"
+    const val andutils = "1.0.32"
+    const val jutils = "1.0.7"
     const val firebaseutils = "1.0.1"
-    const val resutils = "1.0.1"
+    const val resutils = "1.0.3"
     const val rangebar = "1.0.2"
     const val stackoverflowtag = "1.0.5"
 
@@ -61,6 +61,7 @@ object Versions {
     const val buildTimeTracker = "0.11.0"
     const val archLifecycle = "1.1.1"
     const val okLog = "2.3.0"
+    const val archNavigation = "1.0.0-alpha02"
 }
 
 object Libs {
@@ -136,6 +137,9 @@ object Libs {
     const val retrofit2GsonConverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit2}"
     const val retrofit2CoroutineAdapter = "com.jakewharton.retrofit:retrofit2-kotlin-coroutines-experimental-adapter:${Versions.retrofit2CoroutineAdapter}"
     const val okLog = "com.github.simonpercic:oklog3:${Versions.okLog}"
+    const val archNavigationFragment = "android.arch.navigation:navigation-fragment-ktx:${Versions.archNavigation}"
+    const val archNavigationUi = "android.arch.navigation:navigation-ui-ktx:${Versions.archNavigation}"
+    const val archNavigationSafeArgs = "android.arch.navigation:navigation-safe-args-gradle-plugin:${Versions.archNavigation}"
     // testing
     const val jUnit = "junit:junit:${Versions.jUnit}"
     const val testRunner = "com.android.support.test:runner:${Versions.supportTest}"

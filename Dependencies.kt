404ec241ac4ea67d8aca33fe2d2250e56eed6eba
Jürgen Reiser <jr.berlin@web.de>
2018-07-06 23:23:57 +0200

    gms plugin

--- Dependencies.kt
+++ Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val versionName = "1.0"
 
     const val customPlugins = "1.0.3"
-    const val gmsPlugin = "3.1.0"
+    const val gmsPlugin = "4.0.1"
     const val gradlePlugin = "3.1.1"
     const val fabricPlugin = "1.24.4"
     const val hugoPlugin = "1.2.1"

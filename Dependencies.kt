f5f92da4b8ed9bb05a44003ad67c7808d769acfd
Jürgen Reiser <jr.berlin@web.de>
2018-10-07 20:21:16 +0200

    rename android gradle plugin

--- Dependencies.kt
+++ Dependencies.kt
@@ -147,7 +147,7 @@ object Lib {
 
 object Plugins {
     const val custom = "com.jueggs:gradleplugins:${Versions.customPlugins}"
-    const val gradle = "com.android.tools.build:gradle:${Versions.androidGradlePlugin}"
+    const val androidGradle = "com.android.tools.build:gradle:${Versions.androidGradlePlugin}"
     const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
     const val gms = "com.google.gms:google-services:${Versions.gmsPlugin}"
     const val fabric = "io.fabric.tools:gradle:${Versions.fabricPlugin}"

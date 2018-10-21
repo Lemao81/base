a07b6e15251b460b8275493a4cfb8f0b3aa88955
Lemao81 <jr.berlin@web.de>
2018-10-21 02:01:58 +0200

    base bldsrc project file

---
+++ Project.kt
@@ -0,0 +1,29 @@
+object App {
+    const val applicationId = ""
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val compileSdkVersion = 27
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 27
+}
+
+object Module {
+}
+
+object Dimension {
+    const val main = "main"
+}
+
+object BuildType {
+    const val release = "release"
+    const val debug = "debug"
+}
+
+object Paths {
+    const val baseProject = "base-project.gradle"
+    const val baseAndroidLibrary = "../base-android-library.gradle"
+    const val baseJavaLibrary = "../base-java-library.gradle"
+    const val baseKotlinAndroidExtension = "../base-kotlin-android-extension.gradle"
+    const val baseKapt = "../base-kapt.gradle"
+    const val baseDatabinding = "../base-databinding.gradle"
+}

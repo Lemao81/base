48f89ea10938a8786176746031c4e67f2f68e070
Lemao81 <jr.berlin@web.de>
2019-03-08 12:50:00 +0100

    added settings + global.kt files

---
+++ divers/Global.kt
@@ -0,0 +1,8 @@
+package com.jueggs.babylock
+
+import android.os.Build
+
+fun isLollipopOrAbove() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+fun isMarshmallowOrAbove() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+fun isNougatOrAbove() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+fun isOreoOrAbove() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O

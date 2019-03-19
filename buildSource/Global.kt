5ad53b6d85fc12c9a3a10973d83840b9b9ebe1e0
Lemao81 <jr.berlin@web.de>
2019-03-19 08:25:06 +0100

    Refactoring build source files

--- buildSource/Global.kt
+++ buildSource/Global.kt
@@ -1,11 +1,9 @@
-package buildSrc
-
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.*
 
 fun disableLintTasks(project: Project) {
     project.tasks.whenTaskAdded {
-        if (this.name.startsWith("lint"))
+        if (this.name.startsWith("lint")) {
             this.enabled = false
+        }
     }
 }
--- buildSource/Project.kt
+++ buildSource/Project.kt
@@ -1,22 +1,23 @@
-package buildSrc
-
 object App {
     const val applicationId = ""
     const val versionCode = 1
     const val versionName = "1.0"
+}
+
+object Android {
     const val compileSdkVersion = 28
     const val minSdkVersion = 21
     const val targetSdkVersion = 28
 }
 
-object Module {
+object Modules {
 }
 
-object Dimension {
+object Dimensions {
     const val main = "main"
 }
 
-object Flavor {
+object Flavors {
     const val dev = "dev"
     const val prod = "prod"
 }

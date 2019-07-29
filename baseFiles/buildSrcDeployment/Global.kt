f2791046471ea90599509933e39617c120ac9a0f
Lemao81 <jr.berlin@web.de>
2019-07-29 20:17:11 +0200

    add configure android extension global method

--- baseFiles/buildSrcDeployment/Global.kt
+++ baseFiles/buildSrcDeployment/Global.kt
@@ -1,9 +1,34 @@
 import org.gradle.api.Project
+import com.android.build.gradle.BaseExtension
+import org.gradle.api.JavaVersion
 
 fun disableLintTasks(project: Project) {
     project.tasks.whenTaskAdded {
         if (this.name.startsWith("lint")) {
             this.enabled = false
         }
+    }
+}
+
+fun configureAndroidExtension(android: BaseExtension) {
+    android.compileSdkVersion(Android.compileSdkVersion)
+    android.defaultConfig {
+        applicationId = App.applicationId
+        versionCode = App.versionCode
+        versionName = App.versionName
+        minSdkVersion(Android.minSdkVersion)
+        targetSdkVersion(Android.targetSdkVersion)
+
+        multiDexEnabled = true
+        testInstrumentationRunner = Const.androidTestRunner
+        javaCompileOptions { annotationProcessorOptions.includeCompileClasspath = true }
+    }
+
+    android.dexOptions.preDexLibraries = true
+    android.packagingOptions.pickFirst("protobuf.meta")
+
+    android.compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
